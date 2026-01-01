8c07216b6b9c9a67aab17aa26c11943c228ca2c1
alex <252065501+validcookie@users.noreply.github.com>
2026-01-01 14:26:38 -0800

    input

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import random
 
 while True:
-    print("Hello, World!")
-    print("My name is validcookie and I like cookies. My favorite number is", + random.randint(-1000000000000, 1000000000000))
+	print("Hello, World!")
+
+	c = input("what is your favorite color =>")
+
+	print("My name is validcookie, and I like cookies. My favorite color is",c, "too, and my favorite number is", + random.randint(-1000000000000, 1000000000000))
